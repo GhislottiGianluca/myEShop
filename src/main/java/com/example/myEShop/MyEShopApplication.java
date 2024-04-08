fdfd5a729e8c7e8c790c0b1d2687d267469e969a
Gianluca Ghislotti <g.ghislotti@campus.unimib.it>
2024-04-08 12:01:13 +0200

    Code base of the project added

---
+++ src/main/java/com/example/myEShop/MyEShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.myEShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEShopApplication.class, args);
+	}
+
+}
