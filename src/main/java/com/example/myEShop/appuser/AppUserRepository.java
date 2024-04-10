bc9176a01a50793137c2674519af9c7255d6bed3
Gianluca Ghislotti <g.ghislotti@campus.unimib.it>
2024-04-10 16:15:00 +0200

    appuser package added
    UserDetails and UserDetailsService from Spring Security used

---
+++ src/main/java/com/example/myEShop/appuser/AppUserRepository.java
@@ -0,0 +1,16 @@
+package com.example.myEShop.appuser;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+
+
+
+}
